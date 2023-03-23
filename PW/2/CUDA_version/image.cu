0d6aaf512cb36e77530795143dfb4bdf3fe84e33
Yuya <takagif315@gmail.com>
2023-03-23 11:40:52 +0100

    wip

--- PW/2/CUDA_version/image.cu
+++ PW/2/CUDA_version/image.cu
@@ -9,7 +9,9 @@
 
 #include "helper_math.h"
 
-__global__ void process(const cv::cuda::PtrStep<uchar3> src, cv::cuda::PtrStep<uchar3> dst, int rows, int cols )
+__global__ void process(const cv::cuda::PtrStep<uchar3> l_src,
+                        const cv::cuda::PtrStep<uchar3> l_src,
+                        cv::cuda::PtrStep<uchar3> dst, int rows, int cols )
 {
  
   const int dst_x = blockDim.x * blockIdx.x + threadIdx.x;
@@ -35,5 +37,12 @@ void startCUDA ( cv::cuda::GpuMat& src, cv::cuda::GpuMat& dst )
 
   process<<<grid, block>>>(src, dst, dst.rows, dst.cols);
 
+}
+
+void AnaglyphEffect(const cv::cuda::GpuMat& l_src, const cv::cuda::GpuMat& l_src, cv::cuda::GpuMat& dst){
+  const dim3 block(32, 8);
+  const dim3 grid(divU(dst.cols, block.x), divUp(dst.rows, block.y));
+
+  process<<<grid, block>>>(l_src, r_src, dst, dst.rows, dst.cols);
 }
 
